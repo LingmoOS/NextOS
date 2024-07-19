0bc39ef08973d3c299ed595632ddce18765e9b76
lingmo-dream <2422586862@qq.com>
2024-07-19 09:46:33 +0800

    update build

--- package/gcc/gcc.mk
+++ package/gcc/gcc.mk
@@ -14,7 +14,8 @@ ifeq ($(BR2_GCC_VERSION_ARC),y)
 GCC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,gcc,$(GCC_VERSION))
 GCC_SOURCE = gcc-$(GCC_VERSION).tar.gz
 else
-GCC_SITE = $(BR2_GNU_MIRROR:/=)/gcc/gcc-$(GCC_VERSION)
+# GCC_SITE = $(BR2_GNU_MIRROR:/=)/gcc/gcc-$(GCC_VERSION)
+GCC_SITE = https://gcc.gnu.org/pub/gcc/releases/gcc-$(GCC_VERSION)
 GCC_SOURCE = gcc-$(GCC_VERSION).tar.xz
 endif
 
