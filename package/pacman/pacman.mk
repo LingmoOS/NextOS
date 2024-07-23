c2ce98b67daa2abd95f61e0157d79afd4bd9cc08
lingmo-dream <2422586862@qq.com>
2024-07-23 17:43:45 +0800

    [update]Packages

---
+++ package/pacman/pacman.mk
@@ -0,0 +1,8 @@
+# pacman
+PACMAN_SITE = file://$(TOPDIR)/core
+PACMAN_INSTALL_TARGET = yes
+PACMAN_TARBALL = pacman.tar.xz
+PACMAN_SOURCE = ${DNF_TARBALL}
+PACMAN_LICENSE = GPL-2.0
+PACMAN_CPE_ID_VENDOR = pacman
+$(eval $(host-generic-package))
